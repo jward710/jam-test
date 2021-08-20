07ec50b2f01e4380c4b46ada1caf91e453ae94fc
InkDevStudios <InkDevStudios@gmail.com>
2021-08-20 02:43:56 -0500

    changed the server number thing

--- multiplayer game/script/menus/main menu.gd
+++ multiplayer game/script/menus/main menu.gd
@@ -8,14 +8,14 @@ func _on_ButtonHost_pressed():
 	#creating a new server
 	var net = NetworkedMultiplayerENet.new()
 	#sets the server number and amount of player
-	net.create_server(11111, 2)
+	net.create_server(54833, 2)
 	get_tree().set_network_peer(net)
 	print ("hosting")
 
 
 func _on_ButtonJoin_pressed():
 	var net = NetworkedMultiplayerENet.new()
-	net.create_client("127.0.0.1", 11111)
+	net.create_client("26.41.208.87", 54833)
 	get_tree().set_network_peer(net)
 
 func _player_connected(id):
